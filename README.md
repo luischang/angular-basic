f35839271ae2f027ebe866609a146e19b8633725
luischang <luis.chang@outlook.com>
2019-06-07 15:30:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mkia6t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mkia6t)
---
+++ src/app/productos.ts
@@ -0,0 +1,17 @@
+export const productos = [
+  {
+    nombre: 'Huawei P30 PRO',
+    precio: 799,
+    descripcion: 'Excelente smartphone pero sin tienda de aplicaciones'
+  },
+  {
+    nombre: 'Iphone Mini',
+    precio: 699,
+    descripcion: 'Diseño compacto de un iPhone'
+  },
+  {
+    nombre: 'iPhone X',
+    precio: 999,
+    descripcion: 'Excelente equipo para tus aventuras'
+  }
+];
